879bd325e52d283be8c298b5abdc28f8bdb9f64e
osamaOmrann <oo9264667@gmail.com>
2022-09-25 19:03:46 +0200

    Registeration Screen Login Screen Creating Users In Firebase

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/ui/login/login_screen.dart';
+import 'package:chat_app/ui/registeration/register_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        RegisterScreen.routeName: (_) => RegisterScreen(),
+        LoginScreen.routeName: (_) => LoginScreen(),
+      },
+    );
+  }
+}
---
+++ lib/ui/registeration/register_screen.dart
@@ -0,0 +1,158 @@
+import 'package:chat_app/dialogeUtils.dart';
+import 'package:chat_app/ui/login/login_screen.dart';
+import 'package:chat_app/validation_utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  static const String routeName = 'Register Screen';
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  bool securedPassword = true;
+  Widget Eye = Icon(Icons.visibility);
+  var formKye = GlobalKey<FormState>();
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            image: DecorationImage(
+                image: AssetImage('assets/images/background_pattern.png'),
+                fit: BoxFit.fill)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            centerTitle: true,
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            title: Text('Create Account'),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(12),
+            child: Form(
+              key: formKye,
+              child: SingleChildScrollView(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * .25,
+                      ),
+                      TextFormField(
+                        validator: (text) {
+                          if (text == null || text.trim().isEmpty) {
+                            return 'Please Enter Full Name';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Full Name',
+                        ),
+                      ),
+                      TextFormField(
+                        validator: (text) {
+                          if (text == null || text.trim().isEmpty) {
+                            return 'Please Enter User Name';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'User Name',
+                        ),
+                      ),
+                      TextFormField(
+                        controller: emailController,
+                        validator: (text) {
+                          if (text == null || text.trim().isEmpty) {
+                            return 'Please Enter E-mail Adress';
+                          }
+                          if (!ValidationUtils.isValidEmail(text)) {
+                            return 'Plaese Enter Valid Email';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'E-mail Adress',
+                        ),
+                      ),
+                      TextFormField(
+                        controller: passwordController,
+                        validator: (text) {
+                          if (text == null || text.trim().isEmpty) {
+                            return 'Please Enter Password';
+                          }
+                          if (text.length < 6) {
+                            return 'Password Should Be At Least 6 Characters';
+                          }
+                          return null;
+                        },
+                        obscureText: securedPassword,
+                        decoration: InputDecoration(
+                          suffixIcon: InkWell(
+                              onTap: () {
+                                securedPassword = !securedPassword;
+                                if (securedPassword)
+                                  Eye = Icon(Icons.visibility);
+                                else
+                                  Eye = Icon(Icons.visibility_off);
+                                setState(() {});
+                              },
+                              child: Eye),
+                          labelText: 'Password',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          createAccountClicked();
+                        },
+                        child: Text(
+                          'Create Account',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        style: ButtonStyle(
+                            padding:
+                                MaterialStateProperty.all(EdgeInsets.all(12))),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, LoginScreen.routeName);
+                        },
+                        child: Text('Already Have Account?'),
+                      )
+                    ]),
+              ),
+            ),
+          ),
+        ));
+  }
+
+  var authService = FirebaseAuth.instance;
+
+  void createAccountClicked() {
+    if (formKye.currentState?.validate() == false) {
+      return;
+    }
+    showLoading(context, 'Loading...');
+    authService
+        .createUserWithEmailAndPassword(
+            email: emailController.text, password: passwordController.text)
+        .then((userCredential) {
+      hideLoading(context);
+      showMessage(context, userCredential.user?.uid ?? '');
+    }).onError((error, stackTrace) {
+      showMessage(context, error.toString());
+      hideLoading(context);
+    });
+  }
+}
